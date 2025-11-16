c9672c5ff06ea2351e8fb5c506d3d7fd150aa6f9
Lil-Trill <danil.yurin.1990@mail.ru>
2025-11-17 01:14:11 +0500

    change

--- converter/Program.cs
+++ converter/Program.cs
@@ -1,26 +1,120 @@
 ﻿
+
+using System.ComponentModel;
+using System.IO;
+
 internal class Program
 {
-    public static string[] PlacesName = new string[3];
+//    public string[] UnitsName = new string[9]
+//    {
+//        "один",
+//        "два",
+//        "три",
+//        "четыре",
+//        "пять",
+//        "шесть",
+//        "семь",
+//        "восемь",
+//        "девять"
+//    };
+//    public static string[] TenthsName = new string[9]
+//    {
+//        "десять",
+//        "двадцать",
+//        "тридцать",
+//        "сорок",
+//        "пятьдесят",
+//        "шестьдесят",
+//        "семьдесят",
+//        "восемьдесят",
+//        "девяносто"
+//    };
+//    public string[] HundredName = new string[9]
+//    {
+//        "сто",
+//        "двести",
+//        "триста",
+//        "четыреста",
+//        "пятьсот",
+//        "шестьсот",
+//        "семьсот",
+//        "восемьсот",
+//        "девятьсот"
+//    };
+//public string[] PlacesName = new string[4] 
+//    {
+//        "тысяч",
+//        "миллион",
+//        "миллиард",
+//        "триллион"
+//    };
+    //public Dictionary<string, string[]> Declination = new Dictionary<string, string[]>()
+    //{
+    //    { "Иж", ["одна", "две", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять", "десять"]},
+    //    { }
+    //};
+    public List<string>placesNumber = new List<string>();
     private static void Main(string[] args)
     {
-        int[] nums = PlacesNumbers(3454);
-        for (int i = 0; i < nums.Length; i++) 
-        { 
-            Console.WriteLine(nums[i]);
+        Console.WriteLine("Введите целочисленное число, число не должно");
+        string number = Console.ReadLine();
+        List<string> x = TriplesNumbers(number);
+        foreach(string s in x)
+        {
+            Console.WriteLine(s);
         }
     }
-    private static int[] PlacesNumbers(int num)
+    private static bool CheckNum(int number)
+    {
+        if (number.ToString()[0] == '0')
+        {
+            Console.WriteLine("Вы ввели число начинающее с нуля");
+            return true;
+        }
+        return false;
+    }
+
+    public  void SplitNumber(int number) 
+    {
+        if(number.ToString().Length/3 == 0) placesNumber.Add(number.ToString());
+    }
+    public int[] PlacesNumbers(int num)
     {
         int[] places = new int[num.ToString().Length];
         int lenght = num.ToString().Length;
         for (int i = 0; i < lenght; i++)
         {
-            if(num.ToString().Length == 1) places[i] = num;
+            if (num.ToString().Length == 1) places[i] = num;
             int place = num % 10;
             num = num / 10;
             places[i] = place;
         }
         return places;
     }
-}   
+    private static List<string> TriplesNumbers(string places)
+    {
+        int startIndex = places.Length;
+        List<string> triples = new List<string>();
+        
+        while (startIndex > 0)
+        {
+            startIndex = Math.Max(0, startIndex - 3);
+            int length = 3;
+            triples.Add(places.Substring(startIndex, length));
+            
+        }
+        return triples;
+    }
+}
+
+//разделение строки длинна которой не кратна 3
+//string originalString = "abcdefghij";
+//List<string> parts = new List<string>();
+//int startIndex = 0;
+
+//while (startIndex < originalString.Length)
+//{
+//    int length = Math.Min(3, originalString.Length - startIndex);
+//    parts.Add(originalString.Substring(startIndex, length));
+//    startIndex += 3;
+//}
--- converter/obj/Debug/net8.0/converter.AssemblyInfo.cs
+++ converter/obj/Debug/net8.0/converter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("converter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76e53885ae00f55e94c125c396996fa5cd04c679")]
 [assembly: System.Reflection.AssemblyProductAttribute("converter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("converter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
